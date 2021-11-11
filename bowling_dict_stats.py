9d3386f3551c3cdddebb038bb0e41e193c04ad95
rjmclachlan <64862861+rjmclachlan@users.noreply.github.com>
2021-11-11 15:03:34 -0800

    weekly update 11/11

--- bowling_dict_stats.py
+++ bowling_dict_stats.py
@@ -1,6 +1,7 @@
 import statistics
 
 pinole_league_dict = {
+    '110421': [190, 205, 259],
     '102821': [200, 228, 184],
     '102121': [146, 189, 234],
     '101421': [189, 135, 221],
@@ -11,11 +12,11 @@ pinole_league_dict = {
     '090921': [227, 159, 198],
 }
 
-#convert dictionary values into a list
+# convert dictionary values into a list
 pinole_values = list(pinole_league_dict.values())
 pinole_dict_list = []
-for i in range(0,len(pinole_values)):
-    for x in range(0,3):
+for i in range(0, len(pinole_values)):
+    for x in range(0, 3):
         pinole_dict_list.append(pinole_values[i][x])
 
 print("\n****Pinole Valley Thursday League****")
@@ -26,19 +27,19 @@ print(f"Standard Deivation:  {int(statistics.stdev(pinole_dict_list))}")
 print(f"High game:   ", max(pinole_dict_list))
 print(f"Low game:   ", min(pinole_dict_list))
 
-
-earl_league_dict= {
+earl_league_dict = {
     '092621': [234, 279, 256],
-    '102621': [220, 187, 225]
-    }
+    '102621': [220, 187, 225],
+    '110221': [220, 187, 225],
+    '110921': [220, 187, 225],
+}
 
-#convert dictionary values into a list
+# convert dictionary values into a list
 earl_values = list(earl_league_dict.values())
 earl_dict_list = []
-for i in range(0,len(earl_values)):
-    for x in range(0,3):
+for i in range(0, len(earl_values)):
+    for x in range(0, 3):
         earl_dict_list.append(earl_values[i][x])
-
 
 print("\n****Earl Anthony Tuesday League****")
 print(f"Total games:  ", len(earl_dict_list))
@@ -48,8 +49,8 @@ print(f"Standard Deivation:  {int(statistics.stdev(earl_dict_list))}")
 print(f"High game:   ", max(earl_dict_list))
 print(f"Low game:   ", min(earl_dict_list))
 
-#study for later use
-#a = ({k: sum(v) for k, v in pinole_league_dict.items()})
-#b = sum(map(len, pinole_league_dict.values()))
-#ad = ({k: sum(v) for k, v in earl_league_dict.items()})
-#bd = sum(map(len, earl_league_dict.values()))
+# study for later use
+# a = ({k: sum(v) for k, v in pinole_league_dict.items()})
+# b = sum(map(len, pinole_league_dict.values()))
+# ad = ({k: sum(v) for k, v in earl_league_dict.items()})
+# bd = sum(map(len, earl_league_dict.values()))
